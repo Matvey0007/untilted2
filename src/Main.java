97678f72d42c88864cc4c70c769def45e92a513f
matveipeshkov <mpeshkov007@mail.com>
2023-02-13 12:55:14 +0500

    домашка 2

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+
+            var dog = 8.0;
+            var cat = 3.6;
+            var pappers = 763789;
+        System.out.println( dog);
+        System.out.println( cat);
+        System.out.println( pappers);
+
+            dog = dog + 4;
+            cat = cat + 4;
+            pappers = pappers + 4;
+            System.out.println( dog);
+            System.out.println( cat);
+            System.out.println( pappers);
+        dog = dog - 3.5;
+        cat = cat -1.6;
+        pappers = pappers - 7639;
+        System.out.println( dog);
+        System.out.println( cat);
+        System.out.println( pappers);
+        var frend = 19;
+        System.out.println(frend);
+        frend = frend * 2;
+        System.out.println(frend);
+        frend =frend / 7;
+        System.out.println(frend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog +4;
+        System.out.println(frog);
+        var BoxerWeight1  = 78.2;
+        var BoxerWeight2 = 82.7;
+        System.out.println(BoxerWeight1 + BoxerWeight2);
+        var overload = (BoxerWeight2 - BoxerWeight1);
+        System.out.println("Разница " + overload);
+
+
+
+    }
+}
